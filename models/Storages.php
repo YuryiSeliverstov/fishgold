bc5c74c6630458ce97dbd13d4d7b47ae15183604
Yuryi <yurban.nah@gmail.com>
2023-01-17 22:32:15 +0200

    storages, shops

---
+++ models/Storages.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\models;
+
+use Exception;
+use Swagger\Annotations as SWG;
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\db\ActiveQuery;
+use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
+use yii\db\BaseActiveRecord;
+use yii\behaviors\TimestampBehavior;
+
+/**
+ * @SWG\Definition()
+ * @SWG\Property(property="id", type="string")
+ * @SWG\Property(property="name", type="string")
+ * @SWG\Property(property="description", type="string")
+ * @SWG\Property(property="createdAt", type="integer")
+ * @SWG\Property(property="updatedAt", type="integer")
+ */
+class Storages extends ActiveRecord
+{	
+    public static function tableName(): string
+    {
+        return 'storages';
+    }
+
+    public function rules(): array
+    {
+        return [
+			[['name'], 'string','max'=>255],
+            [['description'], 'safe'],
+            [
+                [
+                    'createdAt',
+					'updatedAt'
+                ],
+                'integer'
+            ],
+        ];
+    }
+
+    public function attributeLabels(): array
+    {
+        return [
+            'id' 			=> 	'id',
+            'name' 			=> 	'Наименование',
+            'description' 	=> 	'Описание',
+			'createdAt'		=>	'Дата создания',
+			'updatedAt'		=>	'Дата изменения'
+        ];
+    }
+	
+	public function behaviors(): array
+    {
+        return [
+            [
+                'class' => TimestampBehavior::class,
+                'attributes' => [
+                    BaseActiveRecord::EVENT_BEFORE_INSERT => ['createdAt', 'updatedAt'],
+                    BaseActiveRecord::EVENT_BEFORE_UPDATE => ['updatedAt'],
+                ],
+            ]
+        ];
+    }
+}
